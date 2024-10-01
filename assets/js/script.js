826d5b5ee71121e07c174df64a8baaa2b54defaa
sameerlama5 <sameertamang5@gmail.com>
2024-10-01 16:01:04 +0545

    about and contact page complte

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,5 +40,39 @@ $(window).on("scroll", function() {
         clickable: true,
       },
     });
+
+     //   counter
+  var a = 0;
+  $(window).scroll(function () {
+    if (!$(".achievement")[0]) {
+      return;
+    }
+    var oTop = $(".achievement").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $(".counter").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-count");
+        $({
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
+          },
+
+          {
+            duration: 5000,
+            easing: "linear",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+            },
+          }
+        );
+      });
+      a = 1;
+    }
+  });
   });
   
