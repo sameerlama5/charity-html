36dd377d1b577403247b8ef8bc895835b2e5726b
sameerlama5 <sameertamang5@gmail.com>
2024-10-02 17:40:36 +0545

    changes homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,70 @@
 jQuery(document).ready(function ($) {
-  'use strict';
-    const SCROLL_OFFSET = 250;
+  "use strict";
+  const SCROLL_OFFSET = 250;
 
-// Scroll event listener
-$(window).on("scroll", function() {
-  if ($(this).scrollTop() > SCROLL_OFFSET) {
-    $(".main-header").addClass("sticky");
-  } else {
-    $(".main-header").removeClass("sticky");
-  }
+  // Scroll event listener
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > SCROLL_OFFSET) {
+      $(".main-header").addClass("sticky");
+    } else {
+      $(".main-header").removeClass("sticky");
+    }
+  });
+
+  $(".header-nav .menu").slicknav({
+    appendTo: ".header-offcanvas .offcanvas-body",
+    label: "",
+    allowParentLinks: true,
+    closedSymbol: "",
+    openedSymbol: "",
+    init: function () {
+      $(".slicknav_btn").remove();
+    },
+  });
+  $(".header-nav .menu").slicknav("open");
+
+  const banner = new Swiper(".banner-swiper", {
+    loop: true,
+    speed: 800,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+  });
+
+// doner slider
+  const donner = new Swiper(".donors", {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    centeredSlides: true,
+    loop: true,
+    speed: 800,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  new VenoBox({
+    selector: '.venobox',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane'
 });
-  
-    $(".header-nav .menu").slicknav({
-      appendTo: ".header-offcanvas .offcanvas-body",
-      label: "",
-      allowParentLinks: true,
-      closedSymbol: "",
-      openedSymbol: "",
-      init: function () {
-        $(".slicknav_btn").remove();
-      },
-    });
-    $(".header-nav .menu").slicknav("open");
-  
-    const banner = new Swiper(".banner-swiper", {
-      
-      loop: true,
-      speed: 800,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-
-     //   counter
+  //   counter
   var a = 0;
   $(window).scroll(function () {
     if (!$(".achievement")[0]) {
@@ -75,22 +98,19 @@ $(window).on("scroll", function() {
     }
   });
 
-  $('.show-btn').click(function(event) {
+  $(".show-btn").click(function (event) {
     event.preventDefault();
-    var para = $(this).closest('.para');
-    para.find('.content-para').show();
-    para.find('.show-btn').hide();
-    para.find('.less-btn').show();
-});
-
-$('.less-btn').click(function(event) {
-    event.preventDefault();
-    var para = $(this).closest('.para');
-    para.find('.content-para').hide();
-    para.find('.show-btn').show();
-    para.find('.less-btn').hide();
-});
-
-
+    var para = $(this).closest(".para");
+    para.find(".content-para").show();
+    para.find(".show-btn").hide();
+    para.find(".less-btn").show();
   });
-  
+
+  $(".less-btn").click(function (event) {
+    event.preventDefault();
+    var para = $(this).closest(".para");
+    para.find(".content-para").hide();
+    para.find(".show-btn").show();
+    para.find(".less-btn").hide();
+  });
+});
